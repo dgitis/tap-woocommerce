6e1a2e280819bedaf939a6581ee21f9f7a64202f
Damon Gudaitis <damon@caretjuice.com>
2021-03-22 12:55:50 -0700

    Re-generated intial code wiht Cookiecutter template

--- setup.py
+++ setup.py
@@ -3,18 +3,16 @@ from setuptools import setup
 
 setup(
     name="tap-woocommerce",
-    version="0.1.2",
+    version="0.1.0",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_woocommerce"],
     install_requires=[
-        'attrs==18.1.0',
-        'backoff==1.3.2',
-        'python-dateutil==2.7.3',
-        'requests==2.20.0',
-        'singer-python==5.0.15',
+        # NB: Pin these to a more specific version for tap reliability
+        "singer-python",
+        "requests",
     ],
     entry_points="""
     [console_scripts]
--- tap_woocommerce/__init__.py
+++ tap_woocommerce/__init__.py
@@ -1,259 +1,110 @@
 #!/usr/bin/env python3
-import itertools
 import os
-import sys
-import time
-import re
 import json
-import attr
-import urllib
-import requests
-import backoff
-from requests.auth import HTTPBasicAuth
 import singer
-import singer.metrics as metrics
-from singer import utils
-import datetime
-import dateutil
-from dateutil import parser
+from singer import utils, metadata
+from singer.catalog import Catalog, CatalogEntry
+from singer.schema import Schema
 
 
 REQUIRED_CONFIG_KEYS = ["url", "consumer_key", "consumer_secret", "start_date"]
 LOGGER = singer.get_logger()
 
-CONFIG = {
-    "url": None,
-    "consumer_key": None,
-    "consumer_secret": None,
-    "start_date":None
-}
-
-ENDPOINTS = {
-    "orders":"wp-json/wc/v2/orders?after={0}&orderby=date&order=asc&per_page=100&page={1}"
-}
-
-def get_endpoint(endpoint, kwargs):
-    '''Get the full url for the endpoint'''
-    if endpoint not in ENDPOINTS:
-        raise ValueError("Invalid endpoint {}".format(endpoint))
-    
-    after = urllib.parse.quote(kwargs[0])
-    page = kwargs[1]
-    return CONFIG["url"]+ENDPOINTS[endpoint].format(after,page)
-
-def get_start(STATE, tap_stream_id, bookmark_key):
-    current_bookmark = singer.get_bookmark(STATE, tap_stream_id, bookmark_key)
-    if current_bookmark is None:
-        return CONFIG["start_date"]
-    return current_bookmark
-
-def load_schema(entity):
-    '''Returns the schema for the specified source'''
-    schema = utils.load_json(get_abs_path("schemas/{}.json".format(entity)))
-
-    return schema
-
-def filter_items(item):
-    filtered = {
-        "id":int(item["id"]),
-        "name":str(item["name"]),
-        "product_id":int(item["product_id"]),
-        "variation_id":int(item["variation_id"]),
-        "quantity":int(item["quantity"]),
-        "subtotal":float(item["subtotal"]),
-        "subtotal_tax":float(item["subtotal_tax"]),
-        "total":float(item["total"]),
-        "sku":str(item["sku"]),
-        "price":float(item["price"])
-    }
-    return filtered
-
-def filter_coupons(coupon):
-    filtered = {
-        "id":int(coupon["id"]),
-        "code":str(coupon["code"]),
-        "discount":float(coupon["discount"])
-    }
-    return filtered
-
-def filter_shipping(ship):
-    filtered = {
-        "id":int(ship["id"]),
-        "method_title":str(ship["method_title"]),
-        "method_id":str(ship["method_id"]),
-        "total":float(ship["total"])
-    }
-    return filtered
-
-def filter_order(order):
-    tzinfo = parser.parse(CONFIG["start_date"]).tzinfo
-    if "line_items" in order and len(order["line_items"])>0:
-        line_items = [filter_items(item) for item in order["line_items"]]
-    else:
-        line_items = None
-    if "coupon_lines" in order and len(order["coupon_lines"])>0:
-        coupon_lines = [filter_coupons(coupon) for coupon in order["coupon_lines"]]
-    else:
-        coupon_lines = None
-    if "shippng_lines" in order and len(order["shipping_lines"])>0:
-        shipping_lines = [filter_shipping(ship) for ship in order["shipping_lines"]]
-    else:
-        shipping_lines = None
-
-    filtered = {
-        "order_id":int(order["id"]),
-        "order_key":str(order["order_key"]),
-        "status":str(order["status"]),
-        "date_created":parser.parse(order["date_created"]).replace(tzinfo=tzinfo).isoformat(),
-        "date_modified":parser.parse(order["date_modified"]).replace(tzinfo=tzinfo).isoformat(),
-        "discount_total":float(order["discount_total"]),
-        "shipping_total":float(order["shipping_total"]),
-        "total":float(order["total"]),
-        "line_items":line_items
-    }
-    return filtered
-
-def giveup(exc):
-    return exc.response is not None \
-        and 400 <= exc.response.status_code < 500 \
-        and exc.response.status_code != 429
-
-@utils.backoff((backoff.expo,requests.exceptions.RequestException), giveup)
-@utils.ratelimit(20, 1)
-def gen_request(stream_id, url):
-    with metrics.http_request_timer(stream_id) as timer:
-        resp = requests.get(url, auth=HTTPBasicAuth(CONFIG["consumer_key"], CONFIG["consumer_secret"]))
-        timer.tags[metrics.Tag.http_status_code] = resp.status_code
-        resp.raise_for_status()
-        return resp.json()
-
-
-def sync_orders(STATE, catalog):
-    schema = load_schema("orders")
-    singer.write_schema("orders", schema, ["order_id"])
-
-    start = get_start(STATE, "orders", "last_update")
-    LOGGER.info("Only syncing orders updated since " + start)
-    last_update = start
-    page_number = 1
-    with metrics.record_counter("orders") as counter:
-        while True:
-            endpoint = get_endpoint("orders", [start, page_number])
-            LOGGER.info("GET %s", endpoint)
-            orders = gen_request("orders",endpoint)
-            for order in orders:
-                counter.increment()
-                order = filter_order(order)
-                if("date_created" in order) and (parser.parse(order["date_created"]) > parser.parse(last_update)):
-                    last_update = order["date_created"]
-                singer.write_record("orders", order)
-            if len(orders) < 100:
-                break
-            else:
-                page_number +=1
-    STATE = singer.write_bookmark(STATE, 'orders', 'last_update', last_update) 
-    singer.write_state(STATE)
-    LOGGER.info("Completed Orders Sync")
-    return STATE
-
-@attr.s
-class Stream(object):
-    tap_stream_id = attr.ib()
-    sync = attr.ib()
-
-STREAMS = [
-    Stream("orders", sync_orders)
-]
-
-def get_streams_to_sync(streams, state):
-    '''Get the streams to sync'''
-    current_stream = singer.get_currently_syncing(state)
-    result = streams
-    if current_stream:
-        result = list(itertools.dropwhile(
-            lambda x: x.tap_stream_id != current_stream, streams))
-    if not result:
-        raise Exception("Unknown stream {} in state".format(current_stream))
-    return result
-
-
-def get_selected_streams(remaining_streams, annotated_schema):
-    selected_streams = []
-
-    for stream in remaining_streams:
-        tap_stream_id = stream.tap_stream_id
-        for stream_idx, annotated_stream in enumerate(annotated_schema.streams):
-            if tap_stream_id == annotated_stream.tap_stream_id:
-                schema = annotated_stream.schema
-                if (hasattr(schema, "selected")) and (schema.selected is True):
-                    selected_streams.append(stream)
-
-    return selected_streams
-
-def do_sync(STATE, catalogs):
-    '''Sync the streams that were selected'''
-    remaining_streams = get_streams_to_sync(STREAMS, STATE)
-    selected_streams = get_selected_streams(remaining_streams, catalogs)
-    if len(selected_streams) < 1:
-        LOGGER.info("No Streams selected, please check that you have a schema selected in your catalog")
-        return
-
-    LOGGER.info("Starting sync. Will sync these streams: %s", [stream.tap_stream_id for stream in selected_streams])
-
-    for stream in selected_streams:
-        LOGGER.info("Syncing %s", stream.tap_stream_id)
-        singer.set_currently_syncing(STATE, stream.tap_stream_id)
-        singer.write_state(STATE)
-
-        try:
-            catalog = [cat for cat in catalogs.streams if cat.stream == stream.tap_stream_id][0]
-            STATE = stream.sync(STATE, catalog)
-        except Exception as e:
-            LOGGER.critical(e)
-            raise e
 
 def get_abs_path(path):
-    '''Returns the absolute path'''
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
 
-def load_discovered_schema(stream):
-    '''Attach inclusion automatic to each schema'''
-    schema = load_schema(stream.tap_stream_id)
-    for k in schema['properties']:
-        schema['properties'][k]['inclusion'] = 'automatic'
-    return schema
 
-def discover_schemas():
-    '''Iterate through streams, push to an array and return'''
-    result = {'streams': []}
-    for stream in STREAMS:
-        LOGGER.info('Loading schema for %s', stream.tap_stream_id)
-        result['streams'].append({'stream': stream.tap_stream_id,
-                                  'tap_stream_id': stream.tap_stream_id,
-                                  'schema': load_discovered_schema(stream)})
-    return result
+def load_schemas():
+    """ Load schemas from schemas folder """
+    schemas = {}
+    for filename in os.listdir(get_abs_path('schemas')):
+        path = get_abs_path('schemas') + '/' + filename
+        file_raw = filename.replace('.json', '')
+        with open(path) as file:
+            schemas[file_raw] = Schema.from_dict(json.load(file))
+    return schemas
 
-def do_discover():
-    '''JSON dump the schemas to stdout'''
-    LOGGER.info("Loading Schemas")
-    json.dump(discover_schemas(), sys.stdout, indent=4)
+
+def discover():
+    raw_schemas = load_schemas()
+    streams = []
+    for stream_id, schema in raw_schemas.items():
+        # TODO: populate any metadata and stream's key properties here..
+        stream_metadata = []
+        key_properties = []
+        streams.append(
+            CatalogEntry(
+                tap_stream_id=stream_id,
+                stream=stream_id,
+                schema=schema,
+                key_properties=key_properties,
+                metadata=stream_metadata,
+                replication_key=None,
+                is_view=None,
+                database=None,
+                table=None,
+                row_count=None,
+                stream_alias=None,
+                replication_method=None,
+            )
+        )
+    return Catalog(streams)
+
+
+def sync(config, state, catalog):
+    """ Sync data from tap source """
+    # Loop over selected streams in catalog
+    for stream in catalog.get_selected_streams(state):
+        LOGGER.info("Syncing stream:" + stream.tap_stream_id)
+
+        bookmark_column = stream.replication_key
+        is_sorted = True  # TODO: indicate whether data is sorted ascending on bookmark value
+
+        singer.write_schema(
+            stream_name=stream.tap_stream_id,
+            schema=stream.schema,
+            key_properties=stream.key_properties,
+        )
+
+        # TODO: delete and replace this inline function with your own data retrieval process:
+        tap_data = lambda: [{"id": x, "name": "row${x}"} for x in range(1000)]
+
+        max_bookmark = None
+        for row in tap_data():
+            # TODO: place type conversions or transformations here
+
+            # write one or more rows to the stream:
+            singer.write_records(stream.tap_stream_id, [row])
+            if bookmark_column:
+                if is_sorted:
+                    # update bookmark to latest value
+                    singer.write_state({stream.tap_stream_id: row[bookmark_column]})
+                else:
+                    # if data unsorted, save max value until end of writes
+                    max_bookmark = max(max_bookmark, row[bookmark_column])
+        if bookmark_column and not is_sorted:
+            singer.write_state({stream.tap_stream_id: max_bookmark})
+    return
+
 
 @utils.handle_top_exception(LOGGER)
 def main():
-    '''Entry point'''
+    # Parse command line arguments
     args = utils.parse_args(REQUIRED_CONFIG_KEYS)
 
-    CONFIG.update(args.config)
-    STATE = {}
-
-    if args.state:
-        STATE.update(args.state)
+    # If discover flag was passed, run discovery mode and dump output to stdout
     if args.discover:
-        do_discover()
-    elif args.catalog:
-        do_sync(STATE, args.catalog)
+        catalog = discover()
+        catalog.dump()
+    # Otherwise run in sync mode
     else:
-        LOGGER.info("No Streams were selected")
+        if args.catalog:
+            catalog = args.catalog
+        else:
+            catalog = discover()
+        sync(args.config, args.state, catalog)
+
 
 if __name__ == "__main__":
     main()
